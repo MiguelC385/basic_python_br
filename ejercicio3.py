85965b1ac5563bc437723a07f5888ef46b2a8dad
Miguel Angel <macaicedoc22@ul.edu.co>
2026-08-11 21:59:24 -0500

    ejercicio 3

---
+++ ejercicio3.py
@@ -0,0 +1,7 @@
+num = int(input("Ingrese un numero Entero por favor: "))
+
+if num % 2 == 0: 
+    print("El número es Par")
+else:
+    impar = num + 1
+    print(f"El número dado es Impar, el siguiente número Par es: {impar}")
