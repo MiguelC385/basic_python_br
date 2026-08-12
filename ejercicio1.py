42208cb0bf3221c9ea6a2e3f33fe73d63d9f11cb
Miguel Angel <macaicedoc22@ul.edu.co>
2026-08-11 21:28:01 -0500

    ejercicio 1

---
+++ ejercicio1.py
@@ -0,0 +1,10 @@
+curso = "Progamacion Agentica"
+semestre = 9
+nombre = "Miguel Angel"
+
+print("--- Datos del Estudiante ---")
+print(f"Nombre: {nombre}")
+print(f"Curso: {curso}")
+print(f"Semestre: {semestre}")
+print("--- Frase creada con los Datos ---")
+print(f"Hola me llamo {nombre} y esto es el curso de {curso} de {semestre} semestre")
