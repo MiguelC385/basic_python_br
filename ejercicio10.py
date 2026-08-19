244199bde6dece685e2639f2c9042fbf8bacb465
Miguel Angel <macaicedoc22@ul.edu.co>
2026-08-19 11:11:24 -0500

    ejercicio 10

---
+++ ejercicio10.py
@@ -0,0 +1,8 @@
+numeros = [1,2,3,4,5,6,7,8,9,10]
+print("--- Resultado ---")
+print(f"Lista completa: {numeros}")
+print(f"Primer elemento: {numeros[0]}")
+print(f"Elemento Central: {numeros[4]}")
+print(f"Ultimo elemento: {numeros[-1]}")
+
+print(f"Total de elementos en la lista: {len(numeros)}")
